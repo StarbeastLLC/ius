ef6da8e14e83ed03a0db7f5fc233314b3ab35dbe
Zura Guerra <guerrazuramx@gmail.com>
2016-02-08 10:03:12 -0600

    Send verification token to users

--- apps/bibliotheca/web/controllers/registration_controller.ex
+++ apps/bibliotheca/web/controllers/registration_controller.ex
@@ -1,7 +1,9 @@
 defmodule Bibliotheca.RegistrationController do
   use Bibliotheca.Web, :controller
-
   alias Bibliotheca.User
+
+  @from "postmaster@sandbox9ddf700296ad4bf0a817cedfe2a09d99.mailgun.org"
+  @welcome "Welcome to Lexi!"
 
   def new(conn, _params) do
     changeset = User.changeset(%User{})
@@ -12,7 +14,12 @@ defmodule Bibliotheca.RegistrationController do
   	changeset = Elegua.changeset(%User{}, user_params)
   	|> User.changeset(user_params)
 
-  	Elegua.register(changeset)
+  	Elegua.register(changeset, :verify)
+    verification_token = changeset.params["verification_token"]
+    user_email = changeset.params["email"]
+    content = "Your token: #{verification_token}"
+    Elegua.send_verification_email(user_email, @from, @welcome, {:text, content})
+    
   	redirect conn, to: "/"
   end
 end
