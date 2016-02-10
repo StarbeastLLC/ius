9f51a7eedc614956d4c69ce7b563dd9bab681991
Zura Guerra <guerrazuramx@gmail.com>
2016-02-09 22:38:49 -0600

    Login users

---
+++ apps/bibliotheca/web/controllers/auth_controller.ex
@@ -0,0 +1,25 @@
+defmodule Bibliotheca.AuthController do
+  use Bibliotheca.Web, :controller
+
+  def login(conn, %{"user" => user_params}) do
+    case Elegua.authenticate({:email, user_params["email"]}, user_params["password"]) do
+      {:ok, user} ->
+        conn
+        |> put_session(:user, user.id)
+        |> put_flash(:info, "Welcome back!")
+        |> redirect(to: "/")
+      {:error, :no_user} ->
+        conn
+        |> put_flash(:error, "Please create an account")
+        |> redirect(to: "/register")
+      {:error, :invalid_password} ->
+        conn
+        |> put_flash(:error, "Invalid password")
+        |> redirect(to: "/") 
+      :else ->
+        conn
+        |> put_flash(:error, "Please try again")
+        |> redirect(to: "/") 
+    end
+  end
+end
