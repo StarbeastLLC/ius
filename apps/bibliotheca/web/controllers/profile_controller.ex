0bd88f3d11ddf1aba900dd1bdfc1c6dd1e6e87aa
Zura Guerra <guerrazuramx@gmail.com>
2016-02-16 01:47:55 -0600

    Save and email new password

--- apps/bibliotheca/web/controllers/profile_controller.ex
+++ apps/bibliotheca/web/controllers/profile_controller.ex
@@ -2,6 +2,9 @@ defmodule Bibliotheca.ProfileController do
   use Bibliotheca.Web, :controller
   alias Bibliotheca.User
 
+  @from "postmaster@sandbox9ddf700296ad4bf0a817cedfe2a09d99.mailgun.org"
+  @password_greeting "You changed your Lexi password!"
+
   def profile(conn, _params) do
     id = get_session(conn, :user_id)
     unless id do
@@ -11,7 +14,8 @@ defmodule Bibliotheca.ProfileController do
     end
     user = Repo.get_by(User, id: id)
     changeset = User.changeset(user)
-    render(conn, "profile.html", user: user, changeset: changeset)
+    password_changeset = User.changeset(user)
+    render(conn, "profile.html", user: user, changeset: changeset, password_changeset: password_changeset)
   end
   
   def update_profile(conn, %{"user" => user_params}) do
@@ -28,6 +32,28 @@ defmodule Bibliotheca.ProfileController do
         conn
         |> put_flash(:error, "Please try again!")
         |> render("profile.html", user: user, changeset: changeset)
+    end
+  end
+
+  def new_password(conn, %{"user" => user_params}) do
+    case Elegua.new_password(user_params["email"], user_params["password"]) do
+      {:ok, user} ->
+        verification_token = user.verification_token
+        user_email = user.email
+        content = "Confirm your password change by clicking this link: http://lexi.mx/change-password/#{verification_token}"
+        Elegua.send_verification_email(user_email, @from, @password_greeting, {:text, content})
+
+        conn
+        |> put_flash(:info, "Check your mail to confirm your password change!")
+        |> redirect(to: "/")
+      {:error, :no_user} ->
+        conn
+        |> put_flash(:error, "You don't have an account, create one!")
+        |> redirect(to: "/register")
+      :else ->
+        conn
+        |> put_flash(:error, "Please try again")
+        |> redirect(to: "/")
     end
   end
 end
