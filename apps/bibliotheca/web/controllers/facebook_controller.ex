f4166cc817e798e9799f315edc57aa7c39e49fe6
Zura Guerra <guerrazuramx@gmail.com>
2016-02-18 12:24:25 -0600

    Register new user with Facebook

--- apps/bibliotheca/web/controllers/facebook_controller.ex
+++ apps/bibliotheca/web/controllers/facebook_controller.ex
@@ -1,20 +1,37 @@
 defmodule Bibliotheca.FacebookController do
   use Bibliotheca.Web, :controller
+  import Ecto.Changeset, only: [put_change: 3, change: 2]
   alias Bibliotheca.User
 
-  def auth(conn, ) do
-      
-  end
-
-  defp check_status(user_params) do
+  def auth(conn, user_params) do
     user = Repo.get_by(User, user_params["email"])
     cond do
       user && user.fb_id == user_params["fb_id"] ->
         login(conn, user)
       user ->
-        :bind
-      :else ->
-        :register
+        case bind_account(user, user_params) do
+          {:ok, user} ->
+            conn
+            |> put_session(:user_id, user.id)
+            |> put_flash(:info, "You connected Facebook to your account!")
+            |> redirect(to: "/")
+          :else -> 
+            conn
+            |> put_flash(:error, "Please try again")
+            |> redirect(to: "/")
+        end
+      user == nil ->
+        case register(user_params) do
+          {:ok, user} ->
+            conn
+            |> put_session(:user_id, user.id)
+            |> put_flash(:info, "Welcome to Lexi!")
+            |> redirect(to: "/") 
+          :else -> 
+            conn
+            |> put_flash(:error, "Please try again")
+            |> redirect(to: "/")        
+        end
     end
   end
 
@@ -25,13 +42,21 @@ defmodule Bibliotheca.FacebookController do
     |> redirect(to: "/")
   end
   
-  defp bind_account(conn, user, user_params) do
+  defp bind_account(user, user_params) do
     fb_params = %{
       :fb_id => user_params["fb_id"],
       :fb_token => user_params["fb_token"]
     }
     changeset = change(user, fb_params)
-    case @app_repo.update(changeset) do
+    case Repo.update(changeset) do
+      {:ok, user} -> {:ok, user}
+      {:error, changeset} -> :error
+    end
+  end
+
+  defp register(user_params) do
+    changeset = change(%User{}, user_params)
+    case Repo.insert(changeset) do
       {:ok, user} -> {:ok, user}
       {:error, changeset} -> :error
     end
