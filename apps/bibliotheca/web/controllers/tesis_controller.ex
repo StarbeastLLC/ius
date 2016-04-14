8a2a52017d924f38b47a3d924d8b570b51c7b75e
Zura Guerra <guerrazuramx@gmail.com>
2016-04-14 02:06:51 -0500

    Go to tesis by ius

--- apps/bibliotheca/web/controllers/tesis_controller.ex
+++ apps/bibliotheca/web/controllers/tesis_controller.ex
@@ -1,10 +1,16 @@
 defmodule Bibliotheca.TesisController do
   use Bibliotheca.Web, :controller
   
-  alias Bibliotheca.{Repo, Tesis}
+  alias Bibliotheca.{Repo, Tesis, PageView}
 
   def show(conn, %{"ius" => ius}) do
-    tesis = Repo.get!(Tesis, ius)
-    render(conn, "show.html", tesis: tesis)
+    case tesis = Repo.get(Tesis, ius) do
+      nil -> 
+        conn
+        |> put_flash(:error, "No hay tesis o jurisprudencias con ese ius")
+        |> redirect(to: "/tesis")
+      %Bibliotheca.Tesis{} -> render(conn, "show.html", tesis: tesis)
+    end 
   end
+  def search_ius(conn, %{"search" => ius_params}), do: show(conn, ius_params)
 end
