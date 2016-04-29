4c12a8e6d08f42dc50ef413b92b2aae48dcffc11
Zura Guerra <guerrazuramx@gmail.com>
2016-04-28 23:42:47 -0500

    Parse juris precedentes

--- apps/bibliotheca/web/views/tesis_view.ex
+++ apps/bibliotheca/web/views/tesis_view.ex
@@ -2,6 +2,18 @@ defmodule Bibliotheca.TesisView do
   use Bibliotheca.Web, :view
 
   def separate_amparos(precedentes) do
-    String.replace(precedentes, "Amparo en revisión ", "<br><b>Amparo en revisión</b><br>")
+    terms = [
+            "Amparo directo en revisión",
+            "Amparo directo",
+            "Amparo en revisión",
+            "Amparo indirecto en revisión",
+            "Amparo indirecto",
+            "Contradicción de tesis",
+            "Acción de inconstitucionalidad"
+            ]
+    Enum.reduce(terms, precedentes, fn(term, precedentes) ->
+      styled_term = "<br><b>#{term}</b><br>"
+      String.replace(precedentes, term, styled_term)
+    end)
   end
 end
