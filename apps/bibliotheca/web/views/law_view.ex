c497210f9e7261116937273555c1036e4ae0fbeb
Zura Guerra <guerrazuramx@gmail.com>
2016-05-05 13:38:46 -0500

    Highlight sections and parse them

--- apps/bibliotheca/web/views/law_view.ex
+++ apps/bibliotheca/web/views/law_view.ex
@@ -6,6 +6,44 @@ defmodule Bibliotheca.LawView do
     json
   end
 
-  def real_contents(%{}), do: []
-  def real_contents(contents), do: contents
+  def real_contents(law) do
+    if law.contents != %{} do
+      law.contents["ct"]
+    else
+      []
+    end
+  end
+
+  def style_sections(contents) do
+    contents
+    |> Enum.map(&separate_sections/1)
+    |> Enum.map(&bold_numbers/1)
+  end
+
+  defp separate_sections(section) do
+    terms = [
+            "TITULO",
+            "LIBRO",
+            "CAPITULO"
+            ]
+    Enum.reduce(terms, section, fn(term, section) ->
+      styled_term = "<br><b>#{term}</b>"
+      String.replace(section, term, styled_term)
+    end)
+  end
+
+  defp bold_numbers(section) do
+    terms = [
+            "PRIMERO", "SEGUNDO", "TERCERO", "CUARTO", "QUINTO",
+            "SEXTO", "SEPTIMO", "OCTAVO", "NOVENO", "DECIMO",
+            "I", "II", "III", "IV", "V",
+            "VI", "VII", "VIII", "IX", "X",
+            "XI", "XII", "XIII", "XIV", "XV",
+            "XVI", "XVII", "XVIII", "XIX", "XX"
+            ]
+    Enum.reduce(terms, section, fn(term, section) ->
+      styled_term = "<b>#{term}</b>"
+      String.replace(section, term, styled_term)
+    end)
+  end
 end
