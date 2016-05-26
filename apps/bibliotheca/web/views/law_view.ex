8bc168a5e89e22a1749a82d04f7dd9d9376eaf5d
Zura Guerra <guerrazuramx@gmail.com>
2016-05-26 03:35:52 -0500

    Bold full section in TOC

--- apps/bibliotheca/web/views/law_view.ex
+++ apps/bibliotheca/web/views/law_view.ex
@@ -8,40 +8,7 @@ defmodule Bibliotheca.LawView do
     json
   end
 
-  def style_section(contents) do
-    contents
-    |> separate_sections
-    |> bold_numbers
-  end
-
   def has_search_terms(article, found_articles) do
     Enum.member?(found_articles, to_string(article.id))
-  end
-
-  defp separate_sections(section) do
-    terms = [
-            "TITULO",
-            "LIBRO",
-            "CAPITULO"
-            ]
-    Enum.reduce(terms, section, fn(term, section) ->
-      styled_term = "<br><b>#{term}</b>"
-      String.replace(section, term, styled_term)
-    end)
-  end
-
-  defp bold_numbers(section) do
-    terms = [
-            "UNICO", "PRIMERO", "SEGUNDO", "TERCERO", "CUARTO", "QUINTO",
-            "SEXTO", "SEPTIMO", "OCTAVO", "NOVENO", "DECIMO",
-            "I", "II", "III", "IV", "V",
-            "VI", "VII", "VIII", "IX", "X",
-            "XI", "XII", "XIII", "XIV", "XV",
-            "XVI", "XVII", "XVIII", "XIX", "XX"
-            ]
-    Enum.reduce(terms, section, fn(term, section) ->
-      styled_term = "<b>#{term}</b>"
-      String.replace(section, term, styled_term)
-    end)
   end
 end
