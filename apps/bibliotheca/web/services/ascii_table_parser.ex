40ba36f5acdf4e596f18f9fed0b083427184bdb5
Zura Guerra <guerrazuramx@gmail.com>
2016-06-01 17:54:32 -0500

    Bold ascii table

--- apps/bibliotheca/web/services/ascii_table_parser.ex
+++ apps/bibliotheca/web/services/ascii_table_parser.ex
@@ -1,9 +1,24 @@
 defmodule Bibliotheca.AsciiTableParserService do
+  def parse_table(text) do
+    text
+    |> monospace_font
+    |> bold_table
+  end
+
   defp monospace_font(text) do
     open_span = "<span style=\"font-family: monospace;\">"
     end_span = "</span>"
     text
-    |> String.replace(~r(+---), open_span <> "+---")
-    |> String.replace(~r(---+), "---+" <> end_span)
+    |> String.replace_prefix(~r(+---), open_span <> "+---")
+    |> String.replace_suffix(~r(---+), "---+" <> end_span)
+  end
+
+  defp bold_table(text) do
+    open_bold = "<strong>"
+    end_bold = "</strong>"
+    text
+    |> String.replace("|", open_bold <> "|" <> end_bold)
+    |> String.replace("+", open_bold <> "+" <> end_bold)
+    |> String.replace("-", open_bold <> "-" <> end_bold)
   end
 end
